4e7bdd7fc1c7dc3c115eaf0521a54322e16e26b4
Mark Tasaka <mark.tasaka@gmail.com>
2023-02-18 09:01:39 -0800

    Update 1.01

--- character/dccCleric.php
+++ character/dccCleric.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Dungeon Crawl Classics Cleric Character Generator</title>
+<title>Dungeon Crawl Classics Cleric Character Generator Version 4</title>
  
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     
 	<meta charset="UTF-8">
 	<meta name="description" content="Dungeon Crawl Classics Cleric Character Generator. Goblinoid Games.">
 	<meta name="keywords" content="Dungeon Crawl Classics, Goblinoid Games,HTML5,CSS,JavaScript">
-	<meta name="author" content="Mark Tasaka 2020">
+	<meta name="author" content="Mark Tasaka 2023">
     
-    <link rel="icon" href="../../images/favicon/icon.png" type="image/png" sizes="16x16"> 
+    <link rel="icon" href="../../../../images/favicon/icon.png" type="image/png" sizes="16x16"> 
 		
 
 	<link rel="stylesheet" type="text/css" href="css/cleric.css">
@@ -46,28 +46,75 @@
     include 'php/randomName.php';
     include 'php/clericAlignment.php';
     include 'php/xp.php';
+    include 'php/nameSelect.php';
+    include 'php/gender.php';
     
 
-        if(isset($_POST["theCharacterName"]))
-        {
-            $characterName = $_POST["theCharacterName"];
+    if(isset($_POST["theCharacterName"]))
+    {
+        $characterName = $_POST["theCharacterName"];
+
+    }
+
     
-        }
+    if(isset($_POST["theGivenName"]))
+    {
+        $givenName = $_POST["theGivenName"];
 
-        
-        if(isset($_POST["thePlayerName"]))
-        {
-            $playerName = $_POST["thePlayerName"];
+    }
+
+    if($givenName == '100')
+    {
+        $givenName = rand(0, 49);
+    }
+    else
+    {
+        $givenName = $givenName;
+    }
     
-        }
-        
 
 
+    if(isset($_POST["theSurname"]))
+    {
+        $surname = $_POST["theSurname"];
+
+    }
+
+    if($surname == '100')
+    {
+        $surname = rand(0, 37);
+    }
+    else
+    {
+        $surname = $surname;
+    }
+
+
+
+    if(isset($_POST['theCheckBoxCustomName']) && $_POST['theCheckBoxCustomName'] == 1) 
+    {
+        $givenName = 200;
+        $surname = 200;
         
-        if(isset($_POST["theGender"]))
-        {
-            $gender = $_POST["theGender"];
-        }
+    } 
+    
+    if(isset($_POST["theGender"]))
+    {
+        $gender = $_POST["theGender"];
+    }
+
+    
+    $genderName = getGenderName($gender);
+    $genderNameIdentifier = genderNameGeneration ($gender);
+
+    $fullName = getName($givenName, $surname, $genderNameIdentifier);
+
+
+    if(isset($_POST["thePlayerName"]))
+    {
+        $playerName = $_POST["thePlayerName"];
+
+    }
 
 
         if(isset($_POST['theCheckBoxRandomName']) && $_POST['theCheckBoxRandomName'] == 1) 
@@ -116,12 +163,104 @@
         
         } 
     
-    $dieType = generationMethod ($abilityScoreGen)[0];
-    $numberDie = generationMethod ($abilityScoreGen)[1];
-    $dieRemoved = generationMethod ($abilityScoreGen)[2];
-    $valueAdded = generationMethod ($abilityScoreGen)[3];
-    
     $generationMessage = generationMesssage ($abilityScoreGen);
+            
+    $abilityScoreArray = array();
+
+    if(isset($_POST['theCustomAbilityScore']) && $_POST['theCustomAbilityScore'] == 1) 
+    {        
+        
+        if(isset($_POST["theStrength"]))
+        {
+            $strengthString = $_POST["theStrength"];
+            $strength = intval($strengthString);
+        }      
+
+        if(isset($_POST["theAgility"]))
+        {
+            $agilityString = $_POST["theAgility"];
+            $agility = intval($agilityString);
+        }     
+
+        if(isset($_POST["theStamina"]))
+        {
+            $staminaString = $_POST["theStamina"];
+            $stamina = intval($staminaString);
+        }    
+
+        if(isset($_POST["thePersonality"]))
+        {
+            $personalityString = $_POST["thePersonality"];
+            $personality = intval($personalityString);
+        }  
+
+        if(isset($_POST["theIntelligence"]))
+        {
+            $intelligenceString = $_POST["theIntelligence"];
+            $intelligence = intval($intelligenceString);
+        }  
+
+
+        if(isset($_POST["theLuck"]))
+        {
+            $luckString = $_POST["theLuck"];
+            $luck = intval($luckString);
+        }  
+
+        $generationMessage = "Custom Ability Scores";
+        $optimizeAbilityScoreMessage = "";
+
+    }
+    else
+    {
+        
+        for($i = 0; $i < 6; ++$i)
+        {
+            $abilityScore = rollAbilityScores ($abilityScoreGen);
+
+            array_push($abilityScoreArray, $abilityScore);
+
+        }
+        
+         $generationMessage = generationMesssage ($abilityScoreGen);
+
+        if(isset($_POST['theOptimizeAbilityScore']) && $_POST['theOptimizeAbilityScore'] == 1) 
+        {
+            rsort($abilityScoreArray);
+
+            $strength = $abilityScoreArray[3];
+            $agility = $abilityScoreArray[5];
+            $stamina = $abilityScoreArray[4];
+            $personality = $abilityScoreArray[0];
+            $intelligence = $abilityScoreArray[1];
+            $luck = $abilityScoreArray[2];
+
+            $optimizeAbilityScoreMessage = "<br/>Ability Scores optimized in the order of Per, Int, Luck, Str, Sta, Agi.";
+        }
+        else
+        {
+            $strength = $abilityScoreArray[0];
+            $agility = $abilityScoreArray[1];
+            $stamina = $abilityScoreArray[2];
+            $personality = $abilityScoreArray[3];
+            $intelligence = $abilityScoreArray[4];
+            $luck = $abilityScoreArray[5];
+            
+            $optimizeAbilityScoreMessage = "";
+        } 
+
+    } 
+    
+
+    $strengthMod = getAbilityModifier($strength);
+    $agilityMod = getAbilityModifier($agility);
+    $staminaMod = getAbilityModifier($stamina);
+    $personalityMod = getAbilityModifier($personality);
+    $intelligenceMod = getAbilityModifier($intelligence);
+    $luckMod = getAbilityModifier($luck);
+
+    $nameGenMessage = getNameDescript($givenName, $surname);
+
     
     
         if(isset($_POST["theArmour"]))
@@ -384,6 +523,12 @@
        <span id="characterName">
            <?php
                 echo $characterName;
+           ?>
+        </span>
+        
+       <span id="characterName2">
+           <?php
+                echo $fullName;
            ?>
         </span>
 
@@ -683,7 +828,7 @@
 
        <span id="abilityScoreGeneration">
             <?php
-           echo $generationMessage;
+           echo $generationMessage . $optimizeAbilityScoreMessage . '<br/>' . $nameGenMessage;
            ?>
        </span>
        
@@ -701,20 +846,19 @@
 	 Character() - Cleric Character Constructor
 	*/
 	function Character() {
-        
-        let strength = rollDice(<?php echo $dieType ?> ,<?php echo $numberDie ?>, <?php echo $dieRemoved ?>, <?php echo $valueAdded ?>);
-        let	intelligence = rollDice(<?php echo $dieType ?> ,<?php echo $numberDie ?>, <?php echo $dieRemoved ?>, <?php echo $valueAdded ?>);
-        let	personality = rollDice(<?php echo $dieType ?> ,<?php echo $numberDie ?>, <?php echo $dieRemoved ?>, <?php echo $valueAdded ?>);
-        let agility = rollDice(<?php echo $dieType ?> ,<?php echo $numberDie ?>, <?php echo $dieRemoved ?>, <?php echo $valueAdded ?>);
-        let stamina = rollDice(<?php echo $dieType ?> ,<?php echo $numberDie ?>, <?php echo $dieRemoved ?>, <?php echo $valueAdded ?>);
-        let	luck = rollDice(<?php echo $dieType ?> ,<?php echo $numberDie ?>, <?php echo $dieRemoved ?>, <?php echo $valueAdded ?>);
-        
-        let strengthMod = abilityScoreModifier(strength);
-        let intelligenceMod = abilityScoreModifier(intelligence);
-        let personalityMod = abilityScoreModifier(personality);
-        let agilityMod = abilityScoreModifier(agility);
-        let staminaMod = abilityScoreModifier(stamina);
-        let luckMod = abilityScoreModifier(luck);
+ 
+        let strength = <?php echo $strength ?>;
+        let	intelligence = <?php echo $intelligence ?>;
+        let	personality = <?php echo $personality ?>;
+        let agility = <?php echo $agility ?>;
+        let stamina = <?php echo $stamina ?>;
+        let	luck = <?php echo $luck ?>;
+        let strengthMod = <?php echo $strengthMod ?>;
+        let intelligenceMod = <?php echo $intelligenceMod ?>;
+        let personalityMod = <?php echo $personalityMod ?>;
+        let agilityMod = <?php echo $agilityMod ?>;
+        let staminaMod = <?php echo $staminaMod ?>;
+        let luckMod = <?php echo $luckMod ?>;
         let level = '<?php echo $level ?>';
         let gender = '<?php echo $gender ?>';
         let armour = '<?php echo $armourName ?>';
